490e3d86d0f99fd6f9fd18c2f8f45cf007f38a4e
shabake <45329453@qq.com>
2019-01-20 16:18:37 +0800

    初始化项目

---
+++ GHAttributesLabelDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GHAttributesLabelDemo
+//
+//  Created by zhaozhiwei on 2019/1/20.
+//  Copyright © 2019年 GHome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GHAttributesLabelDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  GHAttributesLabelDemo
+//
+//  Created by zhaozhiwei on 2019/1/20.
+//  Copyright © 2019年 GHome. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
